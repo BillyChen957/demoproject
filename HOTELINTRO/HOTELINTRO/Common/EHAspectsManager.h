273f68982fa590b9257f50852468776dacddd5f9
xin <billychen957@gmail.com>
2017-11-24 11:29:04 +0800

    runtime

---
+++ HOTELINTRO/HOTELINTRO/Common/EHAspectsManager.h
@@ -0,0 +1,13 @@
+//
+//  EHAspectsManager.h
+//  HOTELINTRO
+//
+//  Created by xin on 2017/11/23.
+//  Copyright © 2017年 pasaaa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EHAspectsManager : NSObject
+
+@end
---
+++ HOTELINTRO/HOTELINTRO/Common/EHAspectsManager.m
@@ -0,0 +1,13 @@
+//
+//  EHAspectsManager.m
+//  HOTELINTRO
+//
+//  Created by xin on 2017/11/23.
+//  Copyright © 2017年 pasaaa. All rights reserved.
+//
+
+#import "EHAspectsManager.h"
+
+@implementation EHAspectsManager
+
+@end
